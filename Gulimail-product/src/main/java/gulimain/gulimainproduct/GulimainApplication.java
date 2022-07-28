7873a2da020a0c4618d37f31ffd1347170b3843a
weihongchen <1337058930@qq.com>
2022-07-28 16:10:03 +0800

    项目初始结构创建

---
+++ Gulimail-product/src/main/java/gulimain/gulimainproduct/GulimainApplication.java
@@ -0,0 +1,13 @@
+package gulimain.gulimainproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimainApplication.class, args);
+    }
+
+}
---
+++ Gulimail-product/src/test/java/gulimain/gulimainproduct/GulimainApplicationTests.java
@@ -0,0 +1,13 @@
+package gulimain.gulimainproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
